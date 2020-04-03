6b3fcb5f87df7d1085a8317bf8db10b4386ff5e2
Jake Robins <mendahu@gmail.com>
2020-04-03 14:03:40 -0700

    fix knexfile

--- knexfile.js
+++ knexfile.js
@@ -16,5 +16,37 @@ module.exports = {
     seeds: {
       directory: __dirname + '/db/seeds'
     }
+  },
+
+  demonstration: {
+    client: 'pg',
+    connection: {
+      host : process.env.DB_HOST,
+      user : process.env.DB_USER,
+      password : process.env.DB_PASS,
+      database : process.env.DB_NAME
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host : process.env.DB_HOST,
+      user : process.env.DB_USER,
+      password : process.env.DB_PASS,
+      database : process.env.DB_NAME
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
   }
 };
