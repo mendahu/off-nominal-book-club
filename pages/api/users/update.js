1e326edbc909a0da8dd0d33d865adf47a59fd103
Jake Robins <mendahu@gmail.com>
2020-04-08 11:23:23 -0700

    switched test token to production token through management api

--- pages/api/users/update.js
+++ pages/api/users/update.js
@@ -1,22 +1,17 @@
-import axios from 'axios'
 import auth0 from '../../../lib/auth0';
+import { ManagementClient } from 'auth0'
 
 export default function update(req, res) {
   
   const { userId } = req.body;
-  const apiBaseUrl = 'https://' + process.env.AUTH0_DOMAIN + '/api/v2/users/'
 
-  const generateUpdateConfig = (auth0id, onbcId) => {
-    return {
-      url: apiBaseUrl + auth0id,
-      method: "PATCH",
-      headers: {
-        "Content-Type": 'application/json',
-        "Authorization": `Bearer ${process.env.AUTH0_MANAGEMENT_TOKEN}`
-      },
-      data: { "app_metadata": { "onbc_id": onbcId } }
-    }
-  }
+  const auth0client = new ManagementClient({
+    domain: process.env.AUTH0_DOMAIN,
+    clientId: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    scope: 'update:users update:users_app_metadata'
+  })
+
   
   if (typeof window === 'undefined') {
      return auth0.getSession(req)
@@ -25,7 +20,11 @@ export default function update(req, res) {
         return user.sub;
       })
       .then(auth0sub => {
-        return axios(generateUpdateConfig(auth0sub, userId))
+
+        const params = { "id": auth0sub }
+        const data = { "app_metadata": { "onbc_id": userId } }
+
+        return auth0client.updateUser(params, data)
       })
       .catch(err => console.error(err))
   }
