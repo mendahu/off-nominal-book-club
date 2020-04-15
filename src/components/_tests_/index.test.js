40cf1b9e07c3a7c8f08c771434ead7b24e93db61
Jake Robins <mendahu@gmail.com>
2020-04-15 08:46:21 -0700

    quick test refactor for index

--- src/components/_tests_/index.test.js
+++ src/components/_tests_/index.test.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { render } from "@testing-library/react";
 import App from "../../../pages/index";
 
-it("renders without crashing", () => {
-  render(<App />);
-});
+afterEach(cleanup)
+
+desccribe("App"), () => {
+
+  it("renders without crashing", () => {
+    render(<App />);
+  });
+
+}
