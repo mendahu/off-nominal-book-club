e060c0d1adf7c21a71bcc09b55da18416413312b
Jake Robins <mendahu@gmail.com>
2020-04-19 11:56:58 -0700

    fixed bug with missing module

--- pages/api/auth0/me.js
+++ pages/api/auth0/me.js
@@ -1,10 +1,9 @@
-
+import auth0 from '../../../lib/auth0';
 import userProfileFetcher from '../../../src/helpers/userProfileFetcher'
 
 export default auth0.requireAuthentication(async function me(req, res) {
 
-  const userProfile = userProfileFetcher(req)
-
+  const userProfile = await userProfileFetcher(req)
   return res.end(JSON.stringify(userProfile))
 
 });
