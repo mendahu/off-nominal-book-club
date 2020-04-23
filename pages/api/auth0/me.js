81612ed37670d973b9ca052c9f1e5febbb03fdac
Jake Robins <mendahu@gmail.com>
2020-04-23 14:55:27 -0700

    fixed error with BASEURL and unauthenticated /api/auth0/me so it can return no console errors for un logged in users

--- pages/api/auth0/me.js
+++ pages/api/auth0/me.js
@@ -1,9 +1,9 @@
-import auth0 from '../../../lib/auth0';
+//import auth0 from '../../../lib/auth0';
 import userProfileFetcher from '../../../src/helpers/userProfileFetcher'
 
-export default auth0.requireAuthentication(async function me(req, res) {
+export default async function me(req, res) {
 
   const userProfile = await userProfileFetcher(req)
   return res.end(JSON.stringify(userProfile))
 
-});
+};
