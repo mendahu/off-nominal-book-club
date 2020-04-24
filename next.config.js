e779cc448b2813666477343b52baf60f54dc34b1
Jake Robins <mendahu@gmail.com>
2020-04-23 17:56:38 -0700

    required dotenv in prod

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(new Dotenv({ silent: true }));
+    return {
+      ...config,
+      node: { fs: 'empty' }
+      }
+   },
+  env: {
+    BASEURL: 'https://onbc.ngrok.io'
+  }   
+};
+
