efb666ff504a8117426125ef63502241f805f1cc
Jake Robins <mendahu@gmail.com>
2020-05-09 07:51:08 -0700

    create new env variable for run time to acccount for different patreon token fetch scenarios

--- src/helpers/patreon/tokenFetcher.js
+++ src/helpers/patreon/tokenFetcher.js
@@ -2,9 +2,9 @@ import { updatePatreonData } from '../auth0/auth0User';
 import { oauth as patreonOAuth } from 'patreon'
 
 export default async function patreonTokenFetcher(code, auth0sub, options) {
-  const CLIENT_ID = process.env.PAT_CLIENT_ID
+  const CLIENT_ID = process.env.PAT_CLIENT_ID || process.env.runtime_PAT_CLIENT_ID
   const CLIENT_SECRET = process.env.PAT_CLIENT_SECRET
-  const REDIRECT_URI = process.env.PAT_REDIRECT_URI
+  const REDIRECT_URI = process.env.PAT_REDIRECT_URI || process.env.runtime_PAT_REDIRECT_URI
   const patreonOAuthClient = patreonOAuth(CLIENT_ID, CLIENT_SECRET)
 
   let patToken;
