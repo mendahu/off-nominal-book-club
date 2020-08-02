b2f349d1ef4fd51cbb13209cdc6a4a467fb10faf
Jake Robins <mendahu@gmail.com>
2020-08-02 08:46:55 -0700

    update tests

--- pages/api/auth0/me.js
+++ pages/api/auth0/me.js
@@ -5,9 +5,12 @@ const me = async (req, res) => {
     const userProfile = await userProfileFetcher(req);
     return res.end(JSON.stringify(userProfile));
   } catch (error) {
-    console.error('Error at /me:', error);
-    res.status(error.status).end();
-    throw error;
+    const { status, message, name } = error;
+    return res.status(status).end({
+      status,
+      name,
+      message,
+    });
   }
 };
 
