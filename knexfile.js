c32d0e5f2895ecff83047528f94935fab87274b8
Jake Robins <mendahu@gmail.com>
2020-08-23 16:44:30 -0700

    knexfile update for staging

--- knexfile.js
+++ knexfile.js
@@ -1,36 +1,51 @@
 require('dotenv').config();
 
 module.exports = {
-
   development: {
     client: 'pg',
     connection: {
-      host : process.env.DEV_DB_HOST,
-      user : process.env.DEV_DB_USER,
-      password : process.env.DEV_DB_PASS,
-      database : process.env.DEV_DB_NAME
+      host: process.env.DEV_DB_HOST,
+      user: process.env.DEV_DB_USER,
+      password: process.env.DEV_DB_PASS,
+      database: process.env.DEV_DB_NAME,
     },
     migrations: {
-      directory: __dirname + '/db/migrations'
+      directory: __dirname + '/db/migrations',
     },
     seeds: {
-      directory: __dirname + '/db/seeds'
-    }
+      directory: __dirname + '/db/seeds',
+    },
+  },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      host: process.env.STAGING_DB_HOST,
+      user: process.env.STAGING_DB_USER,
+      password: process.env.STAGING_DB_PASS,
+      database: process.env.STAGING_DB_NAME,
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds',
+    },
   },
 
   production: {
     client: 'pg',
     connection: {
-      host : process.env.DB_HOST,
-      user : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
     },
     migrations: {
-      directory: __dirname + '/db/migrations'
+      directory: __dirname + '/db/migrations',
     },
     seeds: {
-      directory: __dirname + '/db/seeds'
-    }
-  }
+      directory: __dirname + '/db/seeds',
+    },
+  },
 };
