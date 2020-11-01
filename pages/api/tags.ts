eb7693f2e4a0cfb33aa4825132223a2cbdc9e213
Jake Robins <mendahu@gmail.com>
2020-11-01 08:31:23 -0800

    tests for getAllTags

--- pages/api/tags.ts
+++ pages/api/tags.ts
@@ -2,13 +2,23 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { getAllTags } from '../../db/queries/tags';
 import { AutocompleteTag } from '../../src/types/apiTypes';
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export const tags = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+
+  if (method !== 'GET') {
+    return res.status(405).json({
+      error: `Method ${method} Not Allowed`,
+    });
+  }
+
   try {
     const response = await getAllTags();
-    res.status(200).json(response.rows as AutocompleteTag[]);
+    return res.status(200).json(response.rows as AutocompleteTag[]);
   } catch (err) {
-    res.status(500).json({
+    return res.status(500).json({
       message: 'Error reading database.',
     });
   }
 };
+
+export default tags;
