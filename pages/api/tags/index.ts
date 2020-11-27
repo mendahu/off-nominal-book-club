3cefc5c9e87d7981797887ebd1edf440bd560458
Jake Robins <mendahu@gmail.com>
2020-11-27 08:08:01 -0800

    tags API supports sorting by label or count now

--- pages/api/tags/index.ts
+++ pages/api/tags/index.ts
@@ -1,12 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { getAllTags } from '../../../db/queries/tags';
+import { getAllTags, SortBy } from '../../../db/queries/tags';
 import { ApiTag } from '../../../src/types/api/apiTypes';
 
 export const tags = async (
   req: NextApiRequest,
   res: NextApiResponse<ApiTag[] | { message: string }>
 ) => {
-  const { method } = req;
+  const { method, query } = req;
 
   if (method !== 'GET') {
     return res.status(405).json({
@@ -14,9 +14,30 @@ export const tags = async (
     });
   }
 
+  const { sort } = query;
+  let sortBy: SortBy;
+
+  if (Array.isArray(sort)) {
+    return res.status(400).json({
+      message: `Query strings as arrays are not supported.`,
+    });
+  }
+
+  if (sort !== SortBy.count && sort !== SortBy.label && sort !== undefined) {
+    return res.status(400).json({
+      message: `Query string value for sort (${sort}) is not supported.`,
+    });
+  }
+
+  if (sort === undefined) {
+    sortBy = SortBy.label;
+  } else {
+    sortBy = sort as SortBy;
+  }
+
   try {
-    const response = await getAllTags();
-    return res.status(200).json(response.rows);
+    const response = await getAllTags(sortBy);
+    return res.status(200).json(response);
   } catch (err) {
     return res.status(500).json({
       message: 'Error reading database.',
