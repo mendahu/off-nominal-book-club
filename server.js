ca15646d1ed65bca35776c7d26c476f16209abe9
Jake Robins <mendahu@gmail.com>
2020-11-28 20:15:15 -0800

    custom server to force https

---
+++ server.js
@@ -0,0 +1,26 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = process.env.PORT;
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    if (!dev && req.headers['x-forwarded-proto'] != 'https') {
+      const { host } = parse(process.env.BASEURL);
+      res.writeHead(302, {
+        Location: `https://${host}${req.url}`,
+      });
+      res.end();
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
