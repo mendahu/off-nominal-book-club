59f6c40ca489e8c4ac5662741a4b8dabeab463b1
Jake Robins <me@jakerobins.com>
2021-03-02 14:21:19 -0800

    update config vars

--- src/helpers/auth0/auth0User.js
+++ src/helpers/auth0/auth0User.js
@@ -1,7 +1,7 @@
 import { ManagementClient } from "auth0";
 
 const authOptions = {
-  domain: process.env.AUTH0_ISSUER_BASE_URL,
+  domain: process.env.NEXT_PUBLIC_AUTH0_DOMAIN,
   clientId: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
 };
--- src/helpers/sendPasswordReset.ts
+++ src/helpers/sendPasswordReset.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const sendPasswordReset = async (email: string) => {
-  const url = `https://${process.env.AUTH0_ISSUER_BASE_URL}/dbconnections/change_password`;
+  const url = `https://${process.env.NEXT_PUBLIC_AUTH0_DOMAIN}/dbconnections/change_password`;
 
   const data = {
     client_id: process.env.NEXT_PUBLIC_AUTHO_CLIENT_ID,
