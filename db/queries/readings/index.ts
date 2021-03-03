a3ba15144e2349cbadc70441a3ae8c1527966367
Jake Robins <me@jakerobins.com>
2021-03-03 13:24:21 -0800

    new query for Readings

--- db/queries/readings/index.ts
+++ db/queries/readings/index.ts
@@ -27,7 +27,54 @@ export const validateReadingOwner = (readingId: string, userId: number) => {
 };
 
 export const fetchReading = (readingId: string) => {
-  return knex("readings").select("user_id", "book_id").where("id", readingId);
+  return knex.raw(
+    `
+    SELECT 
+    readings.id, 
+    ( SELECT row_to_json(books) 
+      FROM (
+        SELECT 
+          id, 
+          title,
+          google_id,
+          ( SELECT json_agg(authors) 
+            FROM (
+              SELECT name 
+              FROM authors
+              JOIN books_authors ON authors.id = books_authors.author_id
+              WHERE books_authors.book_id = books.id
+            ) authors
+          ) AS authors
+        FROM books
+        WHERE books.id = readings.book_id
+      ) AS books
+    ) AS book,
+    ( SELECT row_to_json(users)
+      FROM (
+        SELECT id, name, CASE WHEN avatar_select='patreon' THEN patreon_avatar_url ELSE gravatar_avatar_url END as avatar
+        FROM users
+        WHERE id = readings.user_id
+      ) users
+    ) AS host,
+    ( SELECT json_agg(member) 
+      FROM (
+        SELECT users_readings.user_id
+        FROM users_readings
+        WHERE users_readings.reading_id = readings.id
+      ) AS member 
+    ) AS members,
+    ( SELECT json_agg(milestone) 
+      FROM (
+        SELECT readings_milestones.id, readings_milestones.date, readings_milestones.label
+        FROM readings_milestones
+        WHERE readings_milestones.readings_id = readings.id
+      ) AS milestone 
+    ) AS milestones
+  FROM readings
+  WHERE readings.id = ?
+    `,
+    readingId
+  );
 };
 
 export const fetchAllReadings = () => {
