2867fccc52b2f2abff82f3846a2e71def6ee5c3f
Jake Robins <me@jakerobins.com>
2021-03-24 15:29:11 -0700

    update copy on Patreon Promote

--- src/pages/BookPage/components/PatronPromote/PatronPromote.tsx
+++ src/pages/BookPage/components/PatronPromote/PatronPromote.tsx
@@ -1,44 +1,52 @@
-import { Grid, Paper, Button, Typography, CardContent } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles';
-import Link from 'next/link'
+import {
+  Grid,
+  Paper,
+  Button,
+  Typography,
+  CardContent,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Link from "next/link";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   header: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(2, 2, 2, 2)
-  }
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(2, 2, 2, 2),
+  },
 }));
 
 const PatronPromote = (props) => {
-
   const classes = useStyles();
 
   return (
     <Grid item xs={12} sm={4}>
       <Paper>
-
         <CardContent className={classes.header}>
-          <Typography 
-            component='h2' 
-            variant='h5'>Subscribe for more!</Typography>
+          <Typography component="h2" variant="h5">
+            Subscribe for more!
+          </Typography>
         </CardContent>
-        
+
         <CardContent>
           <Typography
-            component='p'
-            color='textSecondary'
-            variant='body2'
-            paragraph>Patron users can add tags, rate and review books, and more!</Typography>
+            component="p"
+            color="textSecondary"
+            variant="body2"
+            paragraph
+          >
+            Patron users can add tags, rate and review books, create Readings
+            with friends, and more!
+          </Typography>
           <Link href={`/users/${props.userId}`} passHref>
-            <Button variant='contained' color='primary' component='a'>Connect your Account</Button>
+            <Button variant="contained" color="primary" component="a">
+              Connect your Account
+            </Button>
           </Link>
         </CardContent>
-
       </Paper>
     </Grid>
-  )
-
-  }
+  );
+};
 
 export default PatronPromote;
