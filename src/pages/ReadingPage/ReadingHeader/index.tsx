ef19ec8b4263ee7201d14c8c00b2a111c951c4e0
Jake Robins <me@jakerobins.com>
2021-03-31 09:23:30 -0700

    refactor Reading Header

--- src/pages/ReadingPage/ReadingHeader/index.tsx
+++ src/pages/ReadingPage/ReadingHeader/index.tsx
@@ -1,26 +1,45 @@
 import {
   Avatar,
-  Button,
-  CircularProgress,
+  Card,
+  CardMedia,
   Grid,
   makeStyles,
   Theme,
   Typography,
 } from "@material-ui/core";
-import { useState } from "react";
-import { useBookClubUser } from "../../../../lib/bookClubUser";
-import { BookThumbnail } from "../../../components/BookThumbnail";
-import LayoutComponent from "../../../components/General/LayoutComponent";
-import { useSnackbarContext } from "../../../contexts/SnackbarContext";
 import generateAuthorString from "../../../helpers/generateAuthorString";
 import { ApiReadingBook, ApiReadingHost } from "../../../types/api/apiTypes";
 import Link from "next/link";
 import urlGenerator from "../../../helpers/urlGenerator";
+import MatLink from "@material-ui/core/Link";
+import { generateBookThumbnailUrl } from "../../../helpers/generateBookThumbnailUrl";
 
 const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    height: 200,
+  },
+  card: {
+    display: "flex",
+    alignItems: "stretch",
+  },
+  media: {
+    height: 200,
+    width: 130,
+    flex: "0 0 auto",
+  },
+  details: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    flexGrow: 1,
+    padding: theme.spacing(2),
+  },
   hostContainer: {
     display: "flex",
+    alignItems: "center",
+    marginTop: theme.spacing(1),
   },
+  titleContainer: {},
   hostName: {
     marginLeft: theme.spacing(1),
   },
@@ -29,103 +48,52 @@ const useStyles = makeStyles((theme: Theme) => ({
 export type ReadingHeaderProps = {
   book: ApiReadingBook;
   host: ApiReadingHost;
-  deleteReading: () => Promise<boolean>;
 };
 
 export default function ReadingHeader(props: ReadingHeaderProps) {
   const classes = useStyles();
-  const { user, loading } = useBookClubUser();
-  const { book, host, deleteReading, ...rest } = props;
-  let isOwner = false;
-  const [isDeleting, setIsDeleting] = useState(false);
-  const triggerSnackbar = useSnackbarContext();
 
+  const { book, host } = props;
   const authorString = generateAuthorString(book.authors);
 
-  const handleDelete = () => {
-    setIsDeleting(true);
-    deleteReading().catch((err) => {
-      setIsDeleting(false);
-      triggerSnackbar({
-        active: true,
-        variant: "error",
-        message: "Error deleting the Reading.",
-      });
-    });
-  };
-
-  if (!loading) {
-    isOwner = Number(host.id) === user.onbc_id;
-  }
-
   return (
-    <LayoutComponent {...rest}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography variant="h6" component="h1">
-            Reading for...
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={4} md={3}>
-          <Link
-            href={`/books/${urlGenerator(book.id, authorString, book.title)}`}
-          >
-            <a>
-              <BookThumbnail
-                id={book.google_id}
-                zoom={3}
-                title={book.title}
-                authorString={authorString}
-                width={"100%"}
-              />
-            </a>
-          </Link>
-        </Grid>
-        <Grid item xs={12} sm={8} md={9}>
-          <Typography variant="h5" component="h1">
-            {book.title}
-          </Typography>
-          <Typography variant="body2" component="h2">
-            By {authorString}
-          </Typography>
-        </Grid>
-        <Grid container item xs={12} spacing={1}>
-          <Grid item xs={12}>
-            <Typography variant="body1" component="h2">
-              Hosted by
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <div className={classes.hostContainer}>
-              <Link href={`/users/${host.id}`}>
-                <Avatar alt={host.name} src={host.avatar} />
-              </Link>
-
-              <Typography
-                variant="h6"
-                component="h2"
-                className={classes.hostName}
-              >
-                <Link href={`/users/${host.id}`}>{host.name}</Link>
-              </Typography>
+    <Grid item xs={12}>
+      <Card className={classes.root}>
+        <Link
+          href={`/books/${urlGenerator(book.id, authorString, book.title)}`}
+          passHref
+        >
+          <MatLink color="inherit" underline="none" className={classes.card}>
+            <CardMedia
+              className={classes.media}
+              image={generateBookThumbnailUrl(book.google_id, 1)}
+              title={"Test"}
+            />
+            <div className={classes.details}>
+              <div className={classes.titleContainer}>
+                <Typography component="p" variant="h5">
+                  {book.title}
+                </Typography>
+                <Typography component="p" variant="subtitle1">
+                  by {authorString}
+                </Typography>
+              </div>
+              <div>
+                <div className={classes.hostContainer}>
+                  <Avatar alt={host.name} src={host.avatar} />
+                  <Typography
+                    component="p"
+                    variant="body2"
+                    className={classes.hostName}
+                  >
+                    {host.name}
+                  </Typography>
+                </div>
+              </div>
             </div>
-          </Grid>
-          <Grid item xs={12}>
-            {isOwner && (
-              <Button
-                variant="contained"
-                color="secondary"
-                onClick={handleDelete}
-                startIcon={
-                  isDeleting && <CircularProgress color="inherit" size={20} />
-                }
-              >
-                Cancel Reading
-              </Button>
-            )}
-          </Grid>
-        </Grid>
-      </Grid>
-    </LayoutComponent>
+          </MatLink>
+        </Link>
+      </Card>
+    </Grid>
   );
 }
